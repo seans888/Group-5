b3d24d974f3fcc1d9acc526e4db496d2278f71dd
cfjose <chamberj18@gmail.com>
2016-08-24 05:30:15 +0800

    Uploaded a new version of the system

---
+++ Application/sams v1.6/sst/config/docrequest_sst_delete.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+                ['location'=>'modules/test/delete_docrequest.php',
+                'pre'=>'docrequest_sst_delete.php',
+                'post'=>''],
+               );
+
+$_SESSION['sst']['tasks'] = $config;
---
+++ Application/sams v1.6/sst/config/document_sst_edit.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+                ['location'=>'modules/test/edit_document.php',
+                'pre'=>'document_sst_edit.php',
+                'post'=>''],
+               );
+
+$_SESSION['sst']['tasks'] = $config;
