35006e35ea8894a57ded07e9a3ccd97e5e0febae
cfjose <chamberj18@gmail.com>
2016-08-25 11:41:51 +0800

    New system version

---
+++ Application/sams v1.7/sst/config/docrequirement_sst_edit.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+                ['location'=>'modules/test/edit_docrequirement.php',
+                'pre'=>'docrequirement_sst_edit.php',
+                'post'=>''],
+               );
+
+$_SESSION['sst']['tasks'] = $config;
